cc10227ba2a499d655f94d7e04d0bcee1e3b0fd9
Xun Li <xunli@uchicago.edu>
2021-05-13 20:51:44 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App(props) {
+  return (
+    <div className="App">
+      <h1>Hello JsGeoDa</h1>
+      <h2>version: {props.version}</h2>
+    </div>
+  );
+}
